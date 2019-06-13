af95b7512ada94d231d519dd5f0e6c5ae4252b6c
zeromsi <www.zeromsi2@gmail.com>
2019-06-13 18:12:12 +0600

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String CMD_clusterLogin = "oc login https://console.115.127.24.186.nip.io:8443 -u=shahidul -p=leads@123";
+		String CMD_metrics = "oc adm top node etcd.ngfs.com";
+		Process loginToCluster = Runtime.getRuntime().exec(CMD_clusterLogin);
+		loginToCluster.waitFor();
+		while (true) {
+			Process getMetrics = Runtime.getRuntime().exec(CMD_metrics);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(getMetrics.getInputStream()));
+			reader.readLine();
+			String line = reader.readLine();
+
+			try {
+				int lastIndexOfPercentage = line.lastIndexOf('%');
+				int memoryUsages = Integer.parseInt(line.substring(lastIndexOfPercentage - 2, lastIndexOfPercentage));
+				if (memoryUsages > 90) {
+					Runtime.getRuntime().exec("sshpass -p Open@1234 ssh root@115.127.24.184 init 6 ");
+					Runtime.getRuntime().exec("sshpass -p Open@1234 ssh root@115.127.24.186 init 6 ");
+					Runtime.getRuntime().exec("sshpass -p Open@1234 ssh root@115.127.24.188 init 6 ");
+				}
+			} catch (Exception e) {
+			}
+			System.out.println("Server is on good mode");
+			Thread.sleep(10000 * 60 * 5);
+		}
+
+	}
+}
